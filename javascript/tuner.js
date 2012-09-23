96af7d418adbd96649157ec6709c54904988655c
Craig Spence <craigspence0@gmail.com>
2012-09-23 13:02:18 +1200

    B

--- javascript/tuner.js
+++ javascript/tuner.js
@@ -3,26 +3,50 @@
   var Tuner, root;
 
   Tuner = function() {
-    var audioContext, buffer, downsampleRate, downsamplingFactor, error, fft, hamming, hp, lp, options, sampleRate, success;
+    var analyser, audioContext, buffer, bufferFillSize, bufferFiller, downsampleRate, error, fft, fftSize, hamming, hp, i, lp, options, sampleRate, success, _i, _ref;
     navigator.getUserMedia || (navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia);
     audioContext = new AudioContext();
     sampleRate = audioContext.sampleRate;
-    downsamplingFactor = 8;
-    downsampleRate = sampleRate / downsamplingFactor;
+    downsampleRate = 8000;
+    fftSize = 16384;
+    fft = new FFT(fftSize, downsampleRate);
     hamming = new WindowFunction(DSP.HAMMING);
-    hp = new IIRFilter2(DSP.HIGHPASS, 20, 0.1, sampleRate);
-    lp = new IIRFilter2(DSP.LOWPASS, 8000, 0.1, sampleRate);
-    fft = new FFT(16384, downsampleRate);
-    buffer = [];
+    lp = audioContext.createBiquadFilter();
+    lp.type = lp.LOWPASS;
+    lp.frequency = 20;
+    lp.Q = 0.1;
+    hp = audioContext.createBiquadFilter();
+    hp.type = hp.HIGHPASS;
+    hp.frequency = 4000;
+    hp.Q = 0.1;
+    buffer = new Uint8Array(fftSize / 2);
+    for (i = _i = 0, _ref = fftSize / 2; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+      buffer[i] = 0;
+    }
+    bufferFillSize = 1024;
+    bufferFiller = audioContext.createJavaScriptNode(bufferFillSize, 1, 1);
+    bufferFiller.onaudioprocess = function(e) {
+      var _j, _k, _ref1, _ref2;
+      for (i = _j = bufferFillSize, _ref1 = buffer.length; bufferFillSize <= _ref1 ? _j < _ref1 : _j > _ref1; i = bufferFillSize <= _ref1 ? ++_j : --_j) {
+        buffer[i - bufferFillSize] = buffer[i];
+      }
+      for (i = _k = 0, _ref2 = e.inputs[1].length; 0 <= _ref2 ? _k < _ref2 : _k > _ref2; i = 0 <= _ref2 ? ++_k : --_k) {
+        buffer[buffer.length - (bufferFillSize + i)] = e.inputs[1][i];
+      }
+      return console.log(buffer);
+    };
+    analyser = audioContext.createAnalyser();
     options = {
       audio: true,
       video: false
     };
     success = function(stream) {
-      var analyser, canvas, context, data, noise, noiseCount, src;
+      var canvas, context, data, fillBuffer, noise, noiseCount, src;
       src = audioContext.createMediaStreamSource(stream);
-      analyser = audioContext.createAnalyser();
-      src.connect(analyser);
+      src.connect(lp);
+      lp.connect(hp);
+      hp.connect(bufferFiller);
+      bufferFiller.connect(analyser);
       $('.tuner').removeClass('hidden');
       canvas = $('#tuner_canvas')[0];
       canvas.height = $('.tuner').height();
@@ -30,32 +54,39 @@
       context = canvas.getContext('2d');
       noise = [];
       noiseCount = 0;
+      fillBuffer = function() {};
       data = function() {
-        var average, downsampled, f, i, mag2db, s, time, width, _i, _j, _k, _l, _ref, _ref1, _ref2, _ref3, _ref4, _results;
-        time = new Uint8Array(analyser.fftSize);
-        analyser.getByteTimeDomainData(time);
-        hamming.process(time);
-        downsampled = [];
-        for (s = _i = 0, _ref = time.length; 0 <= _ref ? _i < _ref : _i > _ref; s = _i += downsamplingFactor) {
-          downsampled.push(time[s]);
+        var average, f, mag2db, s, upsampled, width, zeroPad, _j, _k, _l, _m, _ref1, _ref2, _ref3, _ref4, _ref5, _results;
+        hamming.process(buffer);
+        zeroPad = function(a, n) {
+          var _j, _results;
+          _results = [];
+          for (i = _j = 0; 0 <= n ? _j < n : _j > n; i = 0 <= n ? ++_j : --_j) {
+            _results.push(a.push(0));
+          }
+          return _results;
+        };
+        upsampled = [];
+        for (s = _j = 0, _ref1 = buffer.length; 0 <= _ref1 ? _j < _ref1 : _j > _ref1; s = 0 <= _ref1 ? ++_j : --_j) {
+          upsampled.push(buffer[s]);
+          zeroPad(upsampled, 1);
         }
-        fft.forward(downsampled);
+        fft.forward(upsampled);
         if (noiseCount < 10) {
-          for (f = _j = 0, _ref1 = fft.spectrum.length; 0 <= _ref1 ? _j < _ref1 : _j > _ref1; f = 0 <= _ref1 ? ++_j : --_j) {
-            if ((_ref2 = noise[f]) == null) {
+          for (f = _k = 0, _ref2 = fft.spectrum.length; 0 <= _ref2 ? _k < _ref2 : _k > _ref2; f = 0 <= _ref2 ? ++_k : --_k) {
+            if ((_ref3 = noise[f]) == null) {
               noise[f] = [];
             }
             noise[f].push(fft.spectrum[f]);
           }
-          noiseCount++;
-        }
-        if (noiseCount === 10) {
+          return noiseCount++;
+        } else if (noiseCount === 10) {
           average = function(arr) {
             return (_.reduce(arr, (function(sum, next) {
               return sum + next;
             }), 0)) / arr.length;
           };
-          for (f = _k = 0, _ref3 = fft.spectrum.length; 0 <= _ref3 ? _k < _ref3 : _k > _ref3; f = 0 <= _ref3 ? ++_k : --_k) {
+          for (f = _l = 0, _ref4 = fft.spectrum.length; 0 <= _ref4 ? _l < _ref4 : _l > _ref4; f = 0 <= _ref4 ? ++_l : --_l) {
             noise = average(noise[f]);
           }
           return noiseCount = null;
@@ -67,7 +98,7 @@
             return 20 * (Math.log(n) / Math.log(10));
           };
           _results = [];
-          for (i = _l = 10, _ref4 = fft.spectrum.length - 10; 10 <= _ref4 ? _l < _ref4 : _l > _ref4; i = 10 <= _ref4 ? ++_l : --_l) {
+          for (i = _m = 10, _ref5 = fft.spectrum.length - 10; 10 <= _ref5 ? _m < _ref5 : _m > _ref5; i = 10 <= _ref5 ? ++_m : --_m) {
             _results.push(context.fillRect(width * i + 1, canvas.height - 10, width, -mag2db(fft.spectrum[i] - noise[i])));
           }
           return _results;
