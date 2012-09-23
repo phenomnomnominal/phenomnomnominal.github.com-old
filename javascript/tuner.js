5bbf2d103c35b2ce9d2c493e64e4af55494f4332
Craig Spence <craigspence0@gmail.com>
2012-09-23 20:35:53 +1200

    B

--- javascript/tuner.js
+++ javascript/tuner.js
@@ -62,7 +62,7 @@
         return (0.5 * ((left.y - right.y) / (left.y - (2 * peak.y) + right.y)) + peak.x) * (sampleRate / fftSize);
       };
       data = function() {
-        var bufferCopy, downsampled, f, firstFreq, freqWidth, left, newMaxTime, p, peak, peaks, right, s, secondFreq, spectrumPoints, thirdFreq, timeWidth, upsampled, x, _j, _k, _l, _len, _m, _n, _ref, _ref1, _ref2, _results;
+        var bufferCopy, downsampled, f, firstFreq, freqWidth, left, newMaxTime, p, peak, peaks, right, s, secondFreq, spectrumPoints, timeWidth, upsampled, x, _j, _k, _l, _len, _m, _n, _ref, _ref1, _ref2, _ref3, _ref4, _results;
         bufferCopy = (function() {
           var _j, _len, _results;
           _results = [];
@@ -113,7 +113,7 @@
         spectrumPoints = (function() {
           var _m, _ref2, _results;
           _results = [];
-          for (x = _m = 0, _ref2 = fft.spectrum.length; 0 <= _ref2 ? _m < _ref2 : _m > _ref2; x = 0 <= _ref2 ? ++_m : --_m) {
+          for (x = _m = 0, _ref2 = fft.spectrum.length / 2; 0 <= _ref2 ? _m < _ref2 : _m > _ref2; x = 0 <= _ref2 ? ++_m : --_m) {
             _results.push({
               x: x,
               y: fft.spectrum[x]
@@ -138,10 +138,16 @@
           }
         }
         if (peaks.length > 0) {
-          firstFreq = peaks[0].y;
-          secondFreq = peaks[1].y;
-          thirdFreq = peaks[2].y;
-          peak = peaks[0];
+          firstFreq = peaks[0].x;
+          secondFreq = peaks[1].x;
+          peak = null;
+          if ((1.4 < (_ref2 = firstFreq / secondFreq) && _ref2 < 1.6)) {
+            peak = peaks[1];
+          } else if ((1.9 < (_ref3 = firstFreq / secondFreq) && _ref3 < 2.1)) {
+            peak = peaks[1];
+          } else {
+            peak = peaks[0];
+          }
           left = {
             x: peak.x - 1,
             y: fft.spectrum[peak.x - 1]
@@ -156,7 +162,7 @@
         context.fillStyle = '#F77';
         freqWidth = (canvas.width - 100) / (fft.spectrum.length / 4);
         _results = [];
-        for (i = _n = 10, _ref2 = (fft.spectrum.length / 2) - 10; 10 <= _ref2 ? _n < _ref2 : _n > _ref2; i = 10 <= _ref2 ? ++_n : --_n) {
+        for (i = _n = 10, _ref4 = (fft.spectrum.length / 2) - 10; 10 <= _ref4 ? _n < _ref4 : _n > _ref4; i = 10 <= _ref4 ? ++_n : --_n) {
           _results.push(context.fillRect(freqWidth * i, canvas.height / 2, freqWidth, -Math.pow(5 * fft.spectrum[i], 2)));
         }
         return _results;
